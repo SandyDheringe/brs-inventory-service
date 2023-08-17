d5bd87d9763ee2c714d1fa8280c91c8f05d234c4
sandeep <sandeep.ariseup@gmail.com>
2023-08-17 18:19:42 +0530

    Initial commit

---
+++ src/main/java/com/brsinventory/BrsInventoryServiceApplication.java
@@ -0,0 +1,13 @@
+package com.brsinventory;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BrsInventoryServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BrsInventoryServiceApplication.class, args);
+    }
+
+}
